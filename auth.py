b692531293e092b70c17ae472b521e4b54a4287f
Aniket Kumar <aniketkr08903@gmail.com>
2023-06-08 15:46:52 +0530

    file Added

---
+++ auth.py
@@ -0,0 +1,17 @@
+keys = {
+    "spotify": {
+        "username" : "o1rpd4sljkihmslpltjypjgfa",
+        "clientID" : "ed68e9305a0447cebeb21e1a4f96effc",
+        "clientSecret" : "b23f98b3b4da430c8ccb3d5c5722e929",
+        "redirect_uri" : "http://google.com/callback/"
+    },
+    "mail":{
+        "username": "aniketkr08903@gmail.com",
+        "password": "Androxy#1"
+    },
+    "personal":{
+        "name": "Aniket Kumar",
+        "Age": "20",
+        "job title": "Software Engineer"
+    }
+}
